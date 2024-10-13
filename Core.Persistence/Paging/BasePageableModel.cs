f092600d48ecabe5eaf6e2bac47e6d11ec894f74
akaya <taberkkaya@gmail.com>
2024-10-13 03:38:40 +0300

    Add project files.

---
+++ Core.Persistence/Paging/BasePageableModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Persistence.Paging;
+
+public abstract class BasePageableModel
+{
+    int Index { get; }
+    int Size { get; }
+    int Count { get; }
+    int Pages { get; }
+    bool HasPrevious { get; }
+    bool HasNext { get; }
+}

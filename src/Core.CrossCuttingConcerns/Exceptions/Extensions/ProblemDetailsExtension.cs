d668983751f8e764a1163e3c42a6fab8ab18b196
akaya <taberkkaya@gmail.com>
2024-10-17 11:50:18 +0300

    Extensions added

--- src/Core.CrossCuttingConcerns/Exceptions/Extensions/ProblemDetailsExtension.cs
+++ src/Core.CrossCuttingConcerns/Exceptions/Extensions/ProblemDetailsExtension.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
 
-public static class ProblemDetailsExtension
+public static class ProblemDetailsExtensions
 {
     public static string AsJson<TProblemDetail>(this TProblemDetail details)
         where TProblemDetail : ProblemDetails => JsonSerializer.Serialize(details);

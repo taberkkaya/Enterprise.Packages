cbe369e0e42523ab9b274730ac2845f2c19a7cff
akaya <taberkkaya@gmail.com>
2024-10-23 13:42:27 +0300

    InMemory and Redis Cache Implementation

---
+++ src/Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; }
+    bool BypassCache { get; }
+    TimeSpan? SlidingExpiration { get; }
+}
